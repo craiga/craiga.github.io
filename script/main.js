d0d8ef9dbfa63ad9eca022b3697a6e0965f44c48
Craig Anderson <craiga@craiga.id.au>
2017-12-24 16:33:37 +0000

    Customise personal web site.

--- script/main.js
+++ script/main.js
@@ -1,25 +1,3 @@
-function konamiRotate(rotator) {
-  if (rotator === void(0)) {
-    rotator = $({deg: 0});
-  } else {
-    rotator.get(0).deg = 0;
-  }
-
-  return rotator.animate(
-    {deg: 360},
-    {
-      duration: 45 / 60 * 1000,  // 45 RPM
-      easing: 'linear',
-      step: function(x){
-        $('.konami-rotate').css({transform: 'rotate(' + x + 'deg)'});
-      },
-      complete: function(){
-        konamiRotate($('.konami-rotate'), rotator);
-      },
-    }
-  );
-}
-
 Raven.context(function () {
   var keyLog = [];
   var konami = '38,38,40,40,37,39,37,39,66,65';
@@ -27,8 +5,7 @@ Raven.context(function () {
     keyLog.push(e.keyCode);
     if (keyLog.toString().indexOf(konami) >= 0) {
       $(document).unbind('keydown',arguments.callee);
-      konamiRotate();
-      $('.konami-audio').get(0).play();
+      alert('Response to Konami code coming soon.');
     }
   });
 });
